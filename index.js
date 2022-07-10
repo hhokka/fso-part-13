7ce140d1ca0040a792cb16df34ec7e2d4f42b5e6
Hans Hokka <hans.hokka@gmail.com>
2022-07-10 16:34:43 +0300

    13.5

--- index.js
+++ index.js
@@ -1,70 +1,20 @@
-require('dotenv').config()
-const { Sequelize, Model, QueryTypes, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
-})
+app.use('/api/blogs', blogsRouter)
 
-class Blog extends Model { }
-Blog.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    author: {
-        type: DataTypes.TEXT,
-        allowNull: true
-    },
-    url: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    title: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    likes: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-}, {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'blog'
-})
-
-app.get('/api/blogs', async (req, res) => {
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    res.json(blogs)
-})
-app.post('/api/blogs', async (req, res) => {
-    console.log('req.body')
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-    console.log('delete here, req.params.id:', req.params.id)
-
-    const blog = await Blog.destroy({
-        where: { id: req.params.id }
+const start = async () => {
+    await connectToDatabase()
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
     })
+}
 
-    res.json(blog)
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-//console.log('Dan Abramov: "Writing Resilient Components", 0 likes')
+start()

27a47f32ef9b8d277d131d1320651e24a2a01c55
Hans Hokka <hans.hokka@gmail.com>
2022-07-14 16:57:20 +0300

    13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,62 @@
 const router = require('express').Router()
 require('express-async-errors')
-const { Blog } = require('../models')
+const jwt = require('jsonwebtoken')
 
-router.get('/', async (req, res) => {
+const { Blog, User } = require('../models')
+const { SECRET } = require('../util/config')
+
+/* router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
 })
+ */
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll({
+        attributes: { exclude: ['userId'] },
+        include: {
+            model: User,
+            attributes: ['name']
+        }
+    })
+    res.json(blogs)
+})
 
-router.post('/', async (req, res, next) => {
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        try {
+            console.log(authorization.substring(7))
+            console.log(SECRET)
+            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+        } catch (error) {
+            console.log(error)
+            return res.status(401).json({ error: 'token invalid' })
+        }
+    } else {
+        return res.status(401).json({ error: 'token missing' })
+    }
+
+    next()
+}
+
+/* router.post('/', tokenExtractor, async (req, res, next) => {
     try {
         const blog = await Blog.create(req.body)
         res.json(blog)
     } catch (error) {
         next('ERROR IN POST')
         //return res.status(400).json({ error })
+    }
+}) */
+
+router.post('/', tokenExtractor, async (req, res) => {
+    try {
+        const user = await User.findByPk(req.decodedToken.id)
+        const blog = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
+        res.json(blog)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error })
     }
 })
 
